ea0b2dc890596000cec94bd2496e363fe71ced09
Guilherme Novais de Souza <guilhermenovaisdesouza@gmail.com>
2023-11-25 19:11:31 +0000

    feat: creating Vector class

--- include/vetor.hpp
+++ include/vetor.hpp
@@ -1,6 +1,15 @@
 #ifndef VETOR_HPP
 #define VETOR_HPP
 
-
+class Vetor {
+	private:
+		unsigned int x, y;
+	public:
+		Vetor(unsigned int newX, unsigned int newY);
+		unsigned int getX();
+		unsigned int getY();
+		void setX(unsigned int newX);
+		void setY(unsigned int newY);
+};
 
 #endif // VETOR_HPP
--- src/vetor.cpp
+++ src/vetor.cpp
@@ -1,1 +1,22 @@
 #include "../include/vetor.hpp"
+
+Vetor::Vetor(unsigned int newX, unsigned int newY) {
+	x = newX;
+	y = newY;
+}
+
+unsigned int Vetor::getX() {
+	return x;
+}
+
+unsigned int Vetor::getY() {
+	return y;
+}
+
+void Vetor::setX(unsigned int newX) {
+	x = newX;
+}
+
+void Vetor::setY(unsigned int newY) {
+	y = newY;
+}
