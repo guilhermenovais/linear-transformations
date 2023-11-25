ada1a010d3468d9a466032040fe61574f0d084b9
Guilherme Novais de Souza <guilhermenovaisdesouza@gmail.com>
2023-11-25 21:25:17 +0000

    feat: creating Game class

--- include/jogo.hpp
+++ include/jogo.hpp
@@ -1,6 +1,17 @@
 #ifndef JOGO_HPP
 #define JOGO_HPP
 
+#include "seg_tree.hpp"
 
+class Jogo {
+	private:
+		unsigned int instanteMax;
+		SegTree* intervalos;
+	public:
+		Jogo(unsigned int qtdIntantes);
+		~Jogo();
+		void atualiza(unsigned int instante, Matriz* novaTransformacao);
+		void consulta(unsigned int nascimento, unsigned int morte, Vetor* vetor);
+};
 
 #endif // JOGO_HPP
--- src/jogo.cpp
+++ src/jogo.cpp
@@ -1,1 +1,22 @@
 #include "../include/jogo.hpp"
+
+Jogo::Jogo(unsigned int qtdInstantes) {
+	instanteMax = qtdInstantes;
+	intervalos = new SegTree(qtdInstantes);
+}
+
+Jogo::~Jogo() {
+	delete intervalos;
+}
+
+void Jogo::atualiza(unsigned int instante, Matriz* novaTransformacao) {
+	if(instante >= instanteMax) throw "Instante inválido.";
+	else intervalos->atualizaTransformacao(instante, novaTransformacao);
+}
+
+void Jogo::consulta(unsigned int nascimento, unsigned int morte, Vetor* vetor) {
+	if(morte >= instanteMax) throw "Instante inválido.";
+	Matriz* transformacao = intervalos->getTransformacao(nascimento, morte);
+	transformacao->transformaVetor(vetor);
+	delete transformacao;
+}
