c74d9a1ec46f3d9b8c3ff76b43fb6c1dc1d12af2
Guilherme Novais de Souza <guilhermenovaisdesouza@gmail.com>
2023-11-25 20:52:13 +0000

    feat: creating SegTree class

--- include/seg_tree.hpp
+++ include/seg_tree.hpp
@@ -1,6 +1,19 @@
 #ifndef SEG_TREE_HPP
 #define SEG_TREE_HPP
 
+#include "matriz.hpp"
 
+class SegTree {
+	private:
+		Matriz** transformacoes;
+		unsigned int tamanho;
+		void update(int noAtual, int inicioIntervalo, int fimIntervalo, int indice, Matriz* novoValor);
+		Matriz* query(int noAtual, int inicioIntervalo, int fimIntervalo, int inicioBusca, int fimBusca);
+	public:
+		SegTree(unsigned int qtdInstantes);
+		~SegTree();
+		void atualizaTransformacao(unsigned int instante, Matriz* transformacao);
+		Matriz* getTransformacao(unsigned int inicio, unsigned int fim);
+};
 
 #endif // SEG_TREE_HPP
--- src/seg_tree.cpp
+++ src/seg_tree.cpp
@@ -1,1 +1,72 @@
 #include "../include/seg_tree.hpp"
+
+SegTree::SegTree(unsigned int qtdInstantes) {
+	tamanho = qtdInstantes * 4;
+	transformacoes = new Matriz*[tamanho];
+	for(int i = 0; i < tamanho; i++)
+		transformacoes[i] = new Matriz(1, 0, 0, 1);
+}
+
+SegTree::~SegTree() {
+	for(int i = 0; i < tamanho; i++) {
+		delete transformacoes[i];
+	}
+	delete[] transformacoes;
+}
+
+void SegTree::update(int noAtual, int inicioIntervalo, int fimIntervalo, int indice, Matriz* novoValor) {
+	if (inicioIntervalo == fimIntervalo) {
+		// Nó folha
+		delete transformacoes[noAtual];
+		transformacoes[noAtual] = novoValor;
+	} else {
+		int mid = (inicioIntervalo + fimIntervalo) / 2;
+		int filhoEsquerda = 2 * noAtual;
+		int filhoDireita = 2 * noAtual + 1;
+
+		// Atualizar recursivamente os filhos
+		if (indice <= mid) {
+			update(filhoEsquerda, inicioIntervalo, mid, indice, novoValor);
+		} else {
+			update(filhoDireita, mid + 1, fimIntervalo, indice, novoValor);
+		}
+
+		// Atualizar o nó atual com a soma dos filhos
+		Matriz::calculaProduto(transformacoes[filhoEsquerda], transformacoes[filhoDireita], transformacoes[noAtual]);
+	}
+}
+
+void SegTree::atualizaTransformacao(unsigned int instante, Matriz* transformacao) {
+	update(1, 0, tamanho - 1, instante, transformacao);
+}
+
+Matriz* SegTree::query(int noAtual, int inicioIntervalo, int fimIntervalo, int inicioBusca, int fimBusca) {
+	if (fimBusca < inicioIntervalo || fimIntervalo < inicioBusca) {
+		// Intervalo completamente fora da consulta
+		return new Matriz(1, 0, 0, 1);
+	}
+	if (inicioBusca <= inicioIntervalo && fimIntervalo <= fimBusca) {
+		// Intervalo completamente dentro da consulta
+		return new Matriz(transformacoes[noAtual]->getA00(), transformacoes[noAtual]->getA01(), transformacoes[noAtual]->getA10(), transformacoes[noAtual]->getA11());
+	}
+
+	int meio = (inicioIntervalo + fimIntervalo) / 2;
+	int filhoEsquerda = 2 * noAtual;
+	int filhoDireita = 2 * noAtual + 1;
+
+	// Recursivamente buscar nos filhos
+	Matriz* matrizEsquerda = query(filhoEsquerda, inicioIntervalo, meio, inicioBusca, fimBusca);
+	Matriz* matrizDireita = query(filhoDireita, meio + 1, fimIntervalo, inicioBusca, fimBusca);
+	Matriz* resultado = new Matriz(1, 0, 0, 1);
+
+	// Calcula produto
+	Matriz::calculaProduto(matrizEsquerda, matrizDireita, resultado);
+	delete matrizEsquerda;
+	delete matrizDireita;
+
+	return resultado;
+}
+
+Matriz* SegTree::getTransformacao(unsigned int inicio, unsigned int fim) {
+	return query(1, 0, tamanho - 1, inicio, fim);
+}
